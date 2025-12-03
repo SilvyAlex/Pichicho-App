d6997207b603d4328dbaeb403d7c87dfff65a39a
SilvyAlex <svargasc@estud.usfq.edu.ec>
2025-12-02 22:45:10 -0500

    Session / Registro gusrdar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SessionService } from './services/session';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,29 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor(private session: SessionService) {}
+  constructor(private session: SessionService,
+    private router: Router
+  ) {}
   async ngOnInit() {
     try {
-      await this.session.load(); // lo haces async para esperar sin bloquear
+      // 1️⃣ Cargar el perfil guardado en el teléfono
+      await this.session.load();
       console.log('Sesión cargada correctamente');
+
+      // 2️⃣ Revisar si hay perfil
+      const profile = this.session.snapshot;
+
+      if (profile) {
+        // ✅ Ya se registró antes → ir directo al HOME
+        this.router.navigateByUrl('/home', { replaceUrl: true });
+      } else {
+        // ⬅️ No hay perfil → flujo normal: intro1 → registro → intro2 → ...
+        this.router.navigateByUrl('/intro1', { replaceUrl: true });
+      }
     } catch (err) {
       console.warn('Error al cargar sesión:', err);
+      // En caso de error, lo mando a intro1 igual
+      this.router.navigateByUrl('/intro1', { replaceUrl: true });
     }
   }
 }
